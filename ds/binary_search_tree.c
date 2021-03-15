91ca5d34c6687923ff3116d48d25a05dbe120ffa
aufheben68 <ulyuly@protonmail.com>
2021-03-15 02:16:24 +0200

    Update binary_search_tree.c

--- ds/binary_search_tree.c
+++ ds/binary_search_tree.c
@@ -1,32 +1,26 @@
-//DUADIKA DENTRA ANAZITISIS
+//Binary Search Tree
+//https://github.com/aufheben68
 
 #include "treeprint.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-// eisagwgi stoixeiou sto dentro
+
 TreeNode* insertItem(TreeNode *root, int x);
-// psaksimo stoixeiou sto dentro
 bool search(TreeNode* root, int x, int i);
-// katastrofh dentrou
 void destroyTree(TreeNode *t);
-// to megalutero stoixeio tou dentrou
 int max(TreeNode* root);
-//to find ena sugkekrimeno komvo sto dentro
 TreeNode* find(TreeNode* root,int x);
-//vriskei to mikrotero
 TreeNode* findMin(TreeNode* root);
-//vres ton epomeno megalutero
 TreeNode* Getsuccessor(TreeNode* root,int x);
-//vres to megethos twn nodes tou upodentrou
 int size(TreeNode* root);
 
 int main(int argc, char** argv) {
     TreeNode *root = NULL;
 	int maxr,size1;
 	TreeNode *T=NULL;
-    char word[31];  // mia symboloseira me to poly 30 characters;
+    char word[31];
     int x;
     printf("Dwste entoles:\n");
     while (1) {
@@ -34,37 +28,36 @@ int main(int argc, char** argv) {
 	if ((strcmp(word, "insert")==0)) {
 		scanf("%d", &x);
 		root = insertItem(root, x);
-    	// ektypwsi dentrou
     	printTree(root);
 	}else if (strcmp(word, "search")==0) {
 		scanf("%d", &x);
 		bool check = search(root, x, 0);
 		if(check == true){
-		printf("O arithmos vrethike!\n");
+		printf("The number has been found!\n");
 		}
-		else printf("Arithmos den vrethike!\n");
+		else printf("The number has not been found!\n");
 	}else if(strcmp(word,"max")==0){
 		maxr  = max(root);
-		printf("To megisto einai to: %d\n", maxr);
+		printf("The max element is: %d\n", maxr);
 	}else if(strcmp(word,"size")==0){
 		scanf("%d", &x);
 		TreeNode*  T= find(root,x);
 		size1=size(T);
-		printf("To megethos(arithmos) twn nodes tou upodentrou\n");
-		printf("mazi me ton eafto tou einai: %d\n",size1);
+		printf("Size(number) of nodes of the subtree\n");
+		printf("including itself is: %d\n",size1);
 		
 	}else if(strcmp(word,"succ")==0){
 		scanf("%d", &x);
 		TreeNode* successor = Getsuccessor(root,x);
 		if(successor == NULL){
-			printf("Den vrethike megaluteros arithmos.\n");
+			printf("Successor has not been found.\n");
 		}else{
-    		printf("O epomenos megaluteros einai to %d\n", successor->item);
+    		printf("The next successor of the number you have searched is %d\n", successor->item);
 	}
 	} else if (strcmp(word, "exit")==0){
 		destroyTree(root);
 		break;
-	}else printf("Agnwsti entolh. Xanaprospathiste.");
+	}else printf("Unknown command. Please try again.");
 	
 	printf("\n");
     };
@@ -74,16 +67,16 @@ int main(int argc, char** argv) {
 
 
 
-// eisagwgi neou kleidiou sto dentro
+// insert new key on the tree
 TreeNode* insertItem(TreeNode *root, int x) {  
-   TreeNode *v = root;  // v trexon kombos
-   TreeNode *pv = NULL; // pv goneas tou v 
+   TreeNode *v = root;  
+   TreeNode *pv = NULL;  
    while (v != NULL) {  
       pv = v; 
       if (x < v->item) v=v->left;
       else if (x > v->item) v=v->right;
-           else { // yparxei idi, ektypwsi mhnymatos
-                  printf("To %d yparxei idi sto dentro.\n", x);
+           else { 
+                  printf("%d is already stored in the tree.\n", x);
 		  return root;
            }   
    }
@@ -93,20 +86,20 @@ TreeNode* insertItem(TreeNode *root, int x) {
       if (x < pv->item) pv->left=tmp;
          else pv->right=tmp;
    } else root=tmp;
-   return root; // epistrofi rizas dentrou
+   return root;
 }
 
 
-//Sunartisi katastrofhs dentrou me riza t
+//Function that helps us free the memory that the tree has allocated
 void destroyTree(TreeNode *t) {
-    // apodesmeysi mnimis me xrisi postorder diasxisis
+    // postorder
     if (t==NULL) return;
-    destroyTree(t->left);   // diagrafi kombwn aristerou ypodentrou
-    destroyTree(t->right);  // diagrafi kombwn deksiou ypodentrou
-    free(t); // diagrafi kombou t
+    destroyTree(t->left);   
+    destroyTree(t->right); 
+    free(t); 
 }
 
-//Synartisi pou vriskei ena stoixeio sto dentro kai epistrefei kai ta nodes pou elegxthikan
+//Function for searching an element on the tree
 bool search(TreeNode* root, int x, int i){
 	if(root != NULL){
 		i++;
@@ -114,16 +107,16 @@ bool search(TreeNode* root, int x, int i){
 			return search(root->left, x, i);
 		}else if (x > root->item)
 			return search(root->right, x, i);
-			printf("Arithmos nodes pou elegxthikan: %d\n",i);
+			printf("Number of nodes that have been checked: %d\n",i);
 			return true;		
 	}
 	else{
-		printf("Arithmos nodes pou elegxthikan: %d\n", i);
+		printf("Number of nodes that have been checked: %d\n", i);
  		return false;
 	}
 }
 
-//Synartisi max pou vriskei to megisto
+//function for max
 int max(TreeNode* root){
 
 	if(root != NULL){
@@ -134,7 +127,7 @@ int max(TreeNode* root){
 	else return 0;
 }
 
-//Sunartisi find pou vriskei ena node se ena duadiko dentro anazitisis
+//function that finds a node on the tree
 TreeNode* find(TreeNode*root, int x) {
 	if(root == NULL) return NULL;
 	else if(root->item == x) return root;
@@ -142,7 +135,7 @@ TreeNode* find(TreeNode*root, int x) {
 	else return find(root->left,x);
 }
 
-//Sunartisi pou vriskei to mikrotero se ena duadiko dentro anazitisis
+//function for min
 TreeNode* findMin(TreeNode* root) {
 	if(root == NULL) return NULL;
 	while(root->left != NULL)
@@ -150,20 +143,20 @@ TreeNode* findMin(TreeNode* root) {
 	return root;
 }
 	
-//Sunartisi pou vriskei ton epomeno megalutero enos arithmou tou dentrou
+//function for the successor
 TreeNode* Getsuccessor(TreeNode* root,int x) {
-	// Psakse to node se xrono - O(h)
+	// search for node in time - O(h)
 	TreeNode* current = find(root,x);
 	if(current == NULL) return NULL;
-	if(current->right != NULL) {  //Periptwsi 1:Ehei deksi upodentro
+	if(current->right != NULL) {  //In case we have a right subtree
 		return findMin(current->right); // O(h)
 	}
-	else {   //Periptwsi 2:Den ehei deksi upodentro  - O(h)
+	else {   //In case we dont have a right subtree - O(h)
 		TreeNode* successor = NULL;
 		TreeNode* ancestor = root;
 		while(ancestor != current) {
 			if(current->item < ancestor->item) {
-				successor = ancestor; //Afto einai to pio vathu node gia to opoio to current einai sta aristera tou
+				successor = ancestor; 
 				ancestor = ancestor->left;
 			}
 			else
@@ -172,7 +165,7 @@ TreeNode* Getsuccessor(TreeNode* root,int x) {
 		return successor;
 	}
 }	
- //sunartisi pou upologizei to megethos(arithmo)twn komvwn tou upodentrou
+ //recursive function that computes the number of nodes a given subtree has
 int size(TreeNode* T){  
   if (T==NULL)
     return 0;
